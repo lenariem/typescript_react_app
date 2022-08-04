5c6b82260a7f7c1eb085495d03d2bcbdb7b6e0fb
unknown <olena.denysova88@gmail.com>
2022-08-04 17:15:30 +0200

    some styling

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import "./NewTodo.css";
 
 type NewTodoProps = {
     onAddTodo: (todoText: string) => void;
@@ -9,13 +10,14 @@ const NewTodo: React.FC<NewTodoProps> = props => {
 
     const todoSubmitHandler = (event: React.FormEvent) => {
         event.preventDefault();
-        const enteredText = textInputRef.current!.value;
+        let enteredText = "";
+        enteredText = textInputRef.current!.value;
         props.onAddTodo(enteredText);
     };
 
     return (
         <form onSubmit={todoSubmitHandler}>
-            <div>
+            <div className="form-control">
                 <label htmlFor="todo-text">Todo Text</label>
                 <input type="text" id="todo-text" ref={textInputRef} />
             </div>
